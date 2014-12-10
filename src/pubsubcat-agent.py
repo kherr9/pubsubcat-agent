ef60d1fe9c3afb6f106817e721f82a8879e2c297
Kevin Herr <kherr9@gmail.com>
2014-12-09 22:46:52 -0600

    add timeout to ser

--- src/pubsubcat-agent.py
+++ src/pubsubcat-agent.py
@@ -103,7 +103,7 @@ def handle_take_photo(dict):
 
 def handle_read_temp_humidity(dict):
 	logger.info('handling read temp humidity.')
-	ser = serial.Serial('/dev/ttyACM0', 9600)
+	ser = serial.Serial('/dev/ttyACM0', 9600, timeout=5)
 	ser.write("1")
 	response = ser.readline()
 	ser.close()
